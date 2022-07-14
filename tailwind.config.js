489ddddedcf45e8e0fada2b1c5a7447e8b96d581
OCKing <51097023+KDW1@users.noreply.github.com>
2022-07-13 19:54:54 -0500

    Navbar Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
     extend: {},
     display: ['group-hover'],
     fontFamily: {
+      'helvet': ['Helvetica', 'sans-serif'],
+      'dosis': ['Dosis', 'sans-serif'],
+      'arvo': ['Arvo', 'serif'],
       'staat': ['Staatliches', 'cursive'],
       'bebas': ['Bebas Neue', 'cursive'],
       'oswald': ['Oswald', 'sans-serif']
