29ee5ae5c4e5222dbb9fbc82e586798873fa46c2
OCKing <51097023+KDW1@users.noreply.github.com>
2022-07-15 12:33:25 -0500

    Refined Dashboard

--- index.js
+++ index.js
@@ -19,3 +19,162 @@ app.use(bodyParser.json())
 app.use('/', express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 
+<<<<<<< Updated upstream
+=======
+app.use(session({
+  secret: process.env.secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+
+app.get('/', (req, res) => { //Homepage
+  console.log(req.session)
+  res.render('index', { //(condition) ? result1 <!--true : result2 <!--false
+    loggedIn: req.session.loggedIn ?? false,
+    user: req.session.user
+  })
+});
+
+app.get('/dashboard', (req, res) => {
+    console.log(req.session)
+    if(req.session.user) {
+        let userPoints = parseInt(req.session.user.points)
+        let _level
+        console.log(userPoints)
+        let levelMinimums = [15, 50, 125, 1000, 3000, 10000]
+        for(let i = 0; i < levelMinimums.length; i++) {
+          if((userPoints >= levelMinimums[i]) && (userPoints < levelMinimums[i+1])) {
+            console.log("Less than " + levelMinimums[i])
+            _level = i + 2;
+            break;
+          } else if(i == 0) {
+            if(userPoints < levelMinimums[0]) {
+              console.log("Less than " + levelMinimums[0])
+              _level = i + 1;
+              break;
+            }
+          } else if(i == levelMinimums.length - 1) {
+            if(userPoints > levelMinimums[i]) {
+              console.log("Less than " + levelMinimums[i])
+              _level = levelMinimums.length + 1;
+              break;
+            }
+          }
+        }
+        let allAbilities = ['Create new answers', 'Upvote questions and answers', 'Comment under all questions and answers', 'Downvote questions and answers', 'View the upvotes/downvotes of any question or answer', 'Participate in Protection votes', 'Close and Reopen Quesitons']
+        let _abilities = allAbilities.splice(0, _level)
+        let user = req.session.user
+        user.points = userPoints
+        user.level = _level
+        user.abilities = _abilities
+        req.session.user = user
+        console.log(`You are Level ${_level}`)
+        console.log(`You can: ` + _abilities)
+        res.render('dashboard', {
+            loggedIn: req.session.loggedIn,
+            user: req.session.user,
+        })
+    } else {
+        res.redirect('/auth/login')
+    }
+})
+
+
+app.get('/auth/login', (req, res) => {
+  if(req.session.loggedIn) return res.redirect('/')
+
+  res.render('login', {})
+});
+app.get('/auth/signup', (req, res) => {
+  if(req.session.loggedIn) return res.redirect('/')
+
+  res.render('signup', {})
+});
+
+app.post("/auth/signup", (req,res) => {
+  // get form daata
+  const { username, password, email } = req.body
+  if(!username || !password || !email) {
+    console.log("Missing username or password or email")
+    console.log(`Username: ${username}, Password: ${password}, Email: ${email}`)
+    return 
+  }
+
+  // Make sure username is alphanumeric (dashes and underscores allowed)
+  var regex = /^[a-zA-Z0-9-_]+$/;
+  if(username.search(regex) == -1) {
+    return res.render('signup', {
+      error: {msg: "Username must be alphanumeric (dashes and underscores allowed)"}
+    })
+  }
+
+  console.log("Creating user: " + username, password)
+  // create user
+  api.createUser(username, email, password).then(data => {
+    if(data.success) {
+      req.session.loggedIn = true
+      req.session.user = {
+        username: data.user.username,
+        user_id: data.user.user_id,
+        email: data.user.email,
+        points: data.user.points,
+        img: gravatarGen(data.user.email)
+      }
+      res.redirect('/')
+    } else {
+      var err = data.error;
+      if(err == "an item with that \"email\" already exists") err = "Email taken!"
+      else if(err == "an item with that \"username\" already exists") err = "Username taken!";
+
+      res.render('signup', {
+        error: {msg: err}
+      })
+    }
+  });
+
+})
+
+app.post("/auth/login", async (req,res) => {
+  // get form data
+  const { username, password } = req.body
+  if(!username || !password) {
+    console.log("Missing username or password")
+    return
+  }
+
+  var user = await api.getUser(username);
+  if(user.success) {
+    // login user
+    var salt = user.user.salt;
+    api.loginUser(username, password, salt).then(data => {
+
+        if(data.success) {
+            req.session.loggedIn = true
+            req.session.user = {
+                username: user.user.username,
+                user_id: user.user.user_id,
+                email: user.user.email,
+                points: user.user.points,
+                img: gravatarGen(user.user.email)
+            }
+            res.redirect('/')
+        } else {
+            res.render('login', {
+                error: {msg: "Incorrect password"}
+            })
+        }
+    
+    })
+} else {
+  res.render('login', {
+    error: {msg: "Invalid username"}
+  })
+}
+
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+>>>>>>> Stashed changes
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,19 @@ module.exports = {
   content: [],
   theme: {
     extend: {},
+<<<<<<< Updated upstream
+=======
+    display: ['group-hover'],
+    fontFamily: {
+      'helvet': ['Helvetica', 'sans-serif'],
+      'dosis': ['Dosis', 'sans-serif'],
+      'abril': ['Abril Fatface', 'cursive'],
+      'edu': ['Edu SA Beginner', 'cursive'],
+      'staat': ['Staatliches', 'cursive'],
+      'bebas': ['Bebas Neue', 'cursive'],
+      'oswald': ['Oswald', 'sans-serif']
+    }
+>>>>>>> Stashed changes
   },
   plugins: [],
 }
