17ffef1406c9a88b843240a25e910c58199c383f
Coder Gautam <gautamgxtv@gmail.com>
2022-07-25 09:16:45 -0500

    show profile pic

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.use(bodyParser.json())
 app.use('/', express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 
+
 app.use(session({
   secret: process.env.secret,
   resave: false,
